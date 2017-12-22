34fd09f4f1446aa0bd2cf3570a015b35ac35eb87
VolodymyrKladochnyi <kladochnyi.v@gmail.com>
2017-12-22 19:46:18 +0200

    Added slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 import menuFunction from './lib/responsive_menu.js';
+import plusDivs from './lib/slider.js';
 
 document.getElementById('responsive-menu').addEventListener('click', menuFunction);
+
+var myArr = document.getElementsByClassName('img__btn');
+
+for(var i=0; i < myArr.length; i++) {
+  myArr[i].addEventListener('click', plusDivs);
+}
+
